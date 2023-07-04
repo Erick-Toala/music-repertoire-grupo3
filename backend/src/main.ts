3702c4345b9e44eef9bf3d7c7a0e211e7e3c1b3b
Erick Toala <94879384+Erick-Toala@users.noreply.github.com>
2023-07-03 22:50:53 -0500

    subida del directorio src del backend

---
+++ backend/src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const corsOptions: CorsOptions = {
+    origin: 'http://localhost:9000', // Reemplaza con el origen de tu aplicación de Angular
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  };
+  
+  app.enableCors(corsOptions);
+  
+  await app.listen(5000);
+}
+bootstrap();
