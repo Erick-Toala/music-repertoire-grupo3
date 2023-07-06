dedc90490a28feeec5aaa4cf61a98ba1ea58fc4d
Erick Toala <94879384+Erick-Toala@users.noreply.github.com>
2023-07-05 22:39:55 -0500

    Update cancion-list.component.ts

--- frontend/src/app/components/cancion/cancion-list/cancion-list.component.ts
+++ frontend/src/app/components/cancion/cancion-list/cancion-list.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { CancionService } from '../../../services/cancion/cancion.service';
 
 @Component({
   selector: 'app-cancion-list',
   templateUrl: './cancion-list.component.html',
   styleUrls: ['./cancion-list.component.css']
 })
-export class CancionListComponent {
+export class CancionListComponent implements OnInit {
+  canciones: any[] = [];
+
+
+  constructor(private cancionService: CancionService){
+    this.canciones = [];
+  }
+
+  ngOnInit():void {
+    this.getCancions();
+  }
+
+  getCancions() {  
+    this.cancionService.getCancions()
+      .subscribe(
+        res => {
+          this.canciones = Object.values(res);
+          console.log(this.canciones);
+        },
+        err => console.log(err)
+      );
+  }
+
+  deleteCancion(id: string){
+    this.cancionService.deleteCancion(id)
+      .subscribe(
+        res=> {
+          this.getCancions();
+        },
+        err => console.log(err)
+      );
+  }
 
 }
