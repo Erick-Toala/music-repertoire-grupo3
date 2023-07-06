7d9a076becabf04e3ccc2a6030b549f7f934553b
PamelaVeraCASTRO <92119095+PamelaVeraCASTRO@users.noreply.github.com>
2023-07-05 22:32:04 -0500

    creación de interface para canción

---
+++ frontend/src/app/interfaces/Cancion.ts
@@ -0,0 +1,9 @@
+export interface Cancion{
+    _id?: string;
+    nombre:string;
+    duracion:string;
+    idArtista:string;
+    urlPortada:string;
+    urlYouTube:string;
+    fechaLanzamiento:string;
+}
