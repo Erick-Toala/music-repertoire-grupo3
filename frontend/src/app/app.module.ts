4b5d9363c993d11c2817e6c6fa98f0394e0b0ad6
Erick Toala <94879384+Erick-Toala@users.noreply.github.com>
2023-07-14 17:29:36 -0500

    Update app.module.ts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -10,7 +10,11 @@ import { AutorListComponent } from './components/autor/autor-list/autor-list.com
 import { AutorFormComponent } from './components/autor/autor-form/autor-form.component';
 import { CancionListComponent } from './components/cancion/cancion-list/cancion-list.component';
 import { CancionFormComponent } from './components/cancion/cancion-form/cancion-form.component';
-
+import { HomeComponent } from './components/home/home.component';
+import { AlbumListComponent } from './components/album/album-list/album-list.component';
+import { AlbumFormComponent } from './components/album/album-form/album-form.component';
+import { ReseniaComponent } from './components/resenia/resenia.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -19,15 +23,21 @@ import { CancionFormComponent } from './components/cancion/cancion-form/cancion-
     AutorListComponent,
     AutorFormComponent,
     CancionListComponent,
-    CancionFormComponent
+    CancionFormComponent,
+    HomeComponent,
+    AlbumListComponent,
+    AlbumFormComponent,
+    ReseniaComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
